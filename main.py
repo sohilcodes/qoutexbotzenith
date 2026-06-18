0b0908a780977ef3002a14cf296e070ecefd2cd2
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2026-06-18 13:03:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-
-asyncio.create_task(
-    run_bot()
-)
-
-print("BOT STARTED")
+asyncio.create_task(run_bot())
+print("🤖 BOT STARTED")
 
 @app.get("/")
 async def root():
-
 return {
-    "status": "running"
+"status": "running"
+}
+
+@app.get("/health")
+async def health():
+return {
+"ok": True
 }
